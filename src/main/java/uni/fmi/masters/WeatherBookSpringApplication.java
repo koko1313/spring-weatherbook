7f9dcfbb08b0e1dfb68e782b690c5b2a1aa91215
Kaloyan Velichkov <=>
2019-11-10 16:43:24 +0200

    init spring

---
+++ src/main/java/uni/fmi/masters/WeatherBookSpringApplication.java
@@ -0,0 +1,13 @@
+package uni.fmi.masters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBookSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherBookSpringApplication.class, args);
+	}
+
+}
